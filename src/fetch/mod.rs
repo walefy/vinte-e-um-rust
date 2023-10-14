22d321262e9d7e8d79013ab40ad9ac7ced90bef9
walefy <walefyd1@gmail.com>
2023-10-14 18:48:31 -0300

    refactor: add impl add_score

--- src/fetch/mod.rs
+++ src/fetch/mod.rs
@@ -1,15 +1,15 @@
-use reqwest::blocking::Response;
 use reqwest::blocking::get as blocking_get;
+use reqwest::blocking::Response;
 use serde_json::Value;
 
 pub fn get(url: &str) -> Result<Value, String> {
-  let response: Response = blocking_get(url).unwrap();
+    let response: Response = blocking_get(url).unwrap();
 
-  if response.status().is_success() {
-      let json: Value = response.json().unwrap();
-      return Ok(json);
-  }
+    if response.status().is_success() {
+        let json: Value = response.json().unwrap();
+        return Ok(json);
+    }
 
-  let error: String = format!("Um erro ocorreu: {}", response.status());
-  Err(error)
+    let error: String = format!("Um erro ocorreu: {}", response.status());
+    Err(error)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+mod deck;
 mod fetch;
 mod input;
-mod deck;
 
-use deck::shuffle_deck;
 use deck::draw_card;
+use deck::shuffle_deck;
 use input::get_input;
 
 struct Player {
-    score: i32
+    score: i32,
+}
+
+impl Player {
+    fn add_score(&mut self, points: i32) {
+        self.score += points;
+    }
 }
 
 fn get_points(card: String) -> i32 {
     return match card.as_str() {
         "QUEEN" | "JACK" | "KING" => 10,
         "ACE" => 1,
-        _ => card.parse::<i32>().unwrap()
+        _ => card.parse::<i32>().unwrap(),
     };
 }
 
@@ -22,44 +28,69 @@ fn player_gets_card(deck_id: &str, player: &mut Player) {
     let card: String = draw_card(&deck_id);
     let points: i32 = get_points(card);
 
-    player.score += points;
+    player.add_score(points);
 }
 
 fn main() {
-    let mut player1: Player = Player { score: 0 };
-    let mut player2: Player = Player { score: 0 };
+    let mut global_player_score: Player = Player { score: 0 };
+    let mut global_computer_score: Player = Player { score: 0 };
 
-    let deck_id: String = shuffle_deck();
+    let mut control_loop: bool = true;
 
-    for _ in 0..52 {
-        let choice: String = get_input("\n[M] More\n[S] Stop\n>> ");
+    while control_loop {
+        let mut local_player_score: Player = Player { score: 0 };
+        let mut local_computer_score: Player = Player { score: 0 };
 
-        if choice.to_lowercase() == "m" {
-            player_gets_card(&deck_id, &mut player1);
-            println!("Você tem: {} pontos", player1.score);
-            
-            if player1.score > 21 {
-                break;
-            }
-        } else {
-            while player2.score < 17 {
-                player_gets_card(&deck_id, &mut player2);
-                println!("O computador tem: {} pontos", player2.score);
+        let deck_id: String = shuffle_deck();
 
-                if player2.score > 21 {
+        println!("{}", "-".repeat(30));
+        println!("You: {} pontos", global_player_score.score);
+        println!("Computer: {} pontos", global_computer_score.score);
+        println!("{}", "-".repeat(30));
+
+        for _ in 0..52 {
+            let choice: String = get_input("\n[M] More\n[S] Stop\n[E] exit\n>> ");
+
+            if choice.to_lowercase() == "m" {
+                player_gets_card(&deck_id, &mut local_player_score);
+                println!("Você tem: {} pontos", local_player_score.score);
+
+                if local_player_score.score > 21 {
                     break;
                 }
+            } else if choice.to_lowercase() == "e" {
+                println!("Você saiu do jogo!");
+                control_loop = false;
+                break;
+            } else {
+                while local_computer_score.score < 17 {
+                    player_gets_card(&deck_id, &mut local_computer_score);
+                    println!("O computador tem: {} pontos", local_computer_score.score);
+
+                    if local_computer_score.score > 21
+                        || local_computer_score.score > local_player_score.score
+                    {
+                        break;
+                    }
+                }
+                break;
             }
+        }
+
+        if !control_loop {
             break;
         }
-    }
 
-    if (player1.score > player2.score && player1.score <= 21) || player2.score > 21 {
-        println!("Você venceu!");
-    } else if player1.score == player2.score {
-        println!("Empate!");
-    }
-    else {
-        println!("O computador venceu!");
+        if (local_player_score.score > local_computer_score.score && local_player_score.score <= 21)
+            || local_computer_score.score > 21
+        {
+            println!("Você venceu!");
+            global_player_score.add_score(1);
+        } else if local_player_score.score == local_computer_score.score {
+            println!("Empate!");
+        } else {
+            println!("O computador venceu!");
+            global_computer_score.add_score(1);
+        }
     }
 }
